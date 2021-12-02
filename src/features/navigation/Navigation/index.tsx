42fbb9595a69be7ef05e1600b291de3a8c2cfa7c
Júnior Bocelli <juniorbocelli@gmail.com>
2021-12-02 13:51:52 -0300

    Bugfix problem in width screen in mobile version.

--- src/features/navigation/Navigation/index.tsx
+++ src/features/navigation/Navigation/index.tsx
@@ -22,7 +22,7 @@ const Navigation: React.FC<INavigationProps> = ({ children }) => {
 
       <Box
         component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)`, xs: '100%' } }}
       >
         <Toolbar />
 
