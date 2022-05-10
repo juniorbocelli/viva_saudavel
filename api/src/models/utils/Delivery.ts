4add82261fd21990fc4adfb040722f25d0a03cc7
Júnior Bocelli <juniorbocelli@gmail.com>
2022-05-10 10:20:28 -0300

    Create shipping calculate and delivery day calculate in checkout page.

--- api/src/models/utils/Delivery.ts
+++ api/src/models/utils/Delivery.ts
@@ -15,13 +15,13 @@ class Delivery {
     let date = new Date();
 
     if (!this.isDeliveryInWeekends)
-      if (Dates.weekDays.indexOf(day) === 0 || Dates.weekDays.indexOf(day) === 1)
+      if (Dates.weekDays.indexOf(day) === 0 || Dates.weekDays.indexOf(day) === 6)
         throw new Error("Não é permitida entrega aos finais de semana");
 
     // Sum minimal wait time
-    Dates.subDays(date, this.minDaysToFirstDelivery);
+    Dates.sumDays(date, this.minDaysToFirstDelivery);
 
-    let dayOfDelivery = Dates.getNextDay(date, day);
+    const dayOfDelivery = Dates.getNextDay(date, day);
 
     if (!this.isDeliveryInHolidays && !this.isDeliveryInWeekends)
       return Dates.getNextBusinessDay(dayOfDelivery);
--- app/src/screens/client/Checkout/types.ts
+++ app/src/screens/client/Checkout/types.ts
@@ -2,4 +2,5 @@ export interface CheckoutFormData {
   deliveryDay: string;
 };
 
-export type DeliveryDayState = null | string;
+export type DeliveryDayState = null | Date;
+export type ShippingValueState = null | number;
